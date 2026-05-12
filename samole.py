016795bc28ea03fe5fb8a38f99a8f23f036fd672
Gilfeya Reji <gilfeya24@gmail.com>
2026-05-12 12:03:05 +0530

    Update API key environment variable in samole.py

--- samole.py
+++ samole.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 
-genai.configure(api_key=os.environ["AIzaSyDmXE1t0ezu9cR6cMxUEeQC3wLEX5__KdE"])
+genai.configure(api_key=os.environ["Google_api_key"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Hello")
